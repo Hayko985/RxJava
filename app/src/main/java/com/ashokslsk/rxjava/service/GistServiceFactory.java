45230bc3e1f8f1864da414f60074d30fcca0f09b
Hayko985 <30463912+Hayko985@users.noreply.github.com>
2019-01-07 19:30:07 +0400

    Handle SSL Exception

--- app/src/main/java/com/ashokslsk/rxjava/service/GistServiceFactory.java
+++ app/src/main/java/com/ashokslsk/rxjava/service/GistServiceFactory.java
@@ -3,16 +3,26 @@ package com.ashokslsk.rxjava.service;
 import com.google.gson.GsonBuilder;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-/**
- * @author Praveen2Gemini
- */
 
 public class GistServiceFactory {
 
@@ -30,11 +40,38 @@ public class GistServiceFactory {
      * @return
      */
     private OkHttpClient getOkHttpClient() {
-        return new OkHttpClient.Builder()
+        X509TrustManager trustManager = null;
+
+        try {
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init((KeyStore) null);
+            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+            if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+                throw new IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers));
+            }
+            trustManager = (X509TrustManager) trustManagers[0];
+        } catch (KeyStoreException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        OkHttpClient.Builder client = new OkHttpClient.Builder()
                 .connectTimeout(30, TimeUnit.SECONDS) // 30 seconds Connection Timeout
                 .readTimeout(30, TimeUnit.SECONDS) // 60 seconds Read Timeout
-                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)) // Logger for Api call
-                .build();
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)); // Logger for Api call
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLSv1.2");
+            sc.init(null, new TrustManager[]{trustManager}, null);
+            client.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()), trustManager);
+            ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                    .tlsVersions(TlsVersion.TLS_1_2)
+                    .build();
+            client.connectionSpecs(Collections.singletonList(cs));
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+
+        return client.build();
     }
 
     /**
